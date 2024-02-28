6bef6d56f7fa339f6f8063059eff3147919ecd69
gouzinka <gouzinka@gmail.com>
2024-02-28 17:00:47 +0100

    Use node adapter for Heroku deploy

--- src/lib/socket.js
+++ src/lib/socket.js
@@ -1,7 +1,7 @@
 import ioClient from 'socket.io-client';
 
 // Use an environment variable or fallback to localhost
-const SERVER_URL = 'http://localhost:5173';
+const SERVER_URL = process.env.NODE_ENV === 'production' ? import.meta.env.PROD_WS_SERVER_URL : 'http://localhost:5173';
 const socket = ioClient(SERVER_URL);
 
 export const io = socket;
