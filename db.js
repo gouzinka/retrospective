68401a51b1847ad5b0265e68a9e23d44f0d9de5e
gouzinka <gouzinka@gmail.com>
2024-02-28 17:47:30 +0100

    Debug heroku deploy - Specify filetype for import

--- db.js
+++ db.js
@@ -1,8 +1,6 @@
 import { PrismaClient } from '@prisma/client';
 
 // Prevent multiple instances of Prisma Client in development:
-declare const global: NodeJS.Global & { prisma?: PrismaClient };
-
 const prisma = global.prisma || new PrismaClient();
 
 if (process.env.NODE_ENV === 'development') global.prisma = prisma;
